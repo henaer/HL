5f4d71cb75dbc4cc8bac70fbcf08c1d174747122
henaer <65643439+henaer@users.noreply.github.com>
2022-11-23 23:14:07 +0800

    20221123

---
+++ 01_灯具系统_hNL-20221122_v1/code/main/main.ino
@@ -0,0 +1,19 @@
+#include <WiFi.h>
+#include <WiFiclient.h>
+#include <WiFiAP.h>
+
+#define LED
+
+//wifi配网
+esp_ble_gap_set_device_name(TEST_DEVICE_NAME)
+const char *ssid = 
+
+void setup() {
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
